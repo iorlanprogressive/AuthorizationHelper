42cddac66166d6520e0c852d73db07f32b887b43
Matvey Zhelobovsky <i.orlan.progressive@ya.ru>
2024-11-02 18:31:02 +0300

    Implemented method of obtaining Basic Authorization

--- AuthorizationHelper/AuthorizationHelper.cs
+++ AuthorizationHelper/AuthorizationHelper.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
 
 namespace AuthorizationHelper;
 
 public class AuthorizationHelper {
 	public static (string username, string password) GetBasicCredentials(HttpRequest request) {
-		throw new NotImplementedException();
+		if (request.Headers.ContainsKey("Authorization")) {
+			var authHeader = request.Headers["Authorization"].ToString();
+			if (authHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase)) {
+				var encodedCredentials = authHeader.Substring("Basic ".Length).Trim();
+				var decodedCredentials = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCredentials));
+				var credentials = decodedCredentials.Split(':', 2);
+				if (credentials.Length == 2) return (credentials[0], credentials[1]);
+			}
+		}
+
+		return (null, null);
 	}
 }
