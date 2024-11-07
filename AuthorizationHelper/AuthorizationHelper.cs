7123fef3fc2f83e9665eed367c17e04f88d16dcf
Matvey Zhelobovsky <i.orlan.progressive@ya.ru>
2024-11-07 15:51:47 +0300

    Implemented method of obtaining Digest Authorization

--- AuthorizationHelper/AuthorizationHelper.cs
+++ AuthorizationHelper/AuthorizationHelper.cs
@@ -39,6 +39,23 @@ public class AuthorizationHelper {
 
 	public static (string username, string realm, string nonce, string uri, string response, string opaque) GetDigestCredentials
 		(HttpRequest request) {
-		throw new NotImplementedException();
+		if (!request.Headers.ContainsKey("Authorization")) return (null, null, null, null, null, null);
+
+		var authHeader = request.Headers["Authorization"].ToString();
+
+		if (!authHeader.StartsWith("Digest ", StringComparison.OrdinalIgnoreCase)) return (null, null, null, null, null, null);
+
+		var digestValues = authHeader.Substring(7).Trim().Split(',');
+
+		var digestDict = digestValues.Select(value => value.Split('=')).ToDictionary(pair => pair[0].Trim(), pair => pair[1].Trim(' ', '"'));
+
+		digestDict.TryGetValue("username", out var username);
+		digestDict.TryGetValue("realm", out var realm);
+		digestDict.TryGetValue("nonce", out var nonce);
+		digestDict.TryGetValue("uri", out var uri);
+		digestDict.TryGetValue("response", out var response);
+		digestDict.TryGetValue("opaque", out var opaque);
+
+		return (username, realm, nonce, uri, response, opaque);
 	}
 }
