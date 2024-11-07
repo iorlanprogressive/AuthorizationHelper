e691645b993d2cde30aa28838a50bf5d8769c0a7
Matvey Zhelobovsky <i.orlan.progressive@ya.ru>
2024-11-07 16:21:01 +0300

    Implemented logging

--- AuthorizationHelper/AuthorizationHelper.cs
+++ AuthorizationHelper/AuthorizationHelper.cs
@@ -7,26 +7,43 @@ namespace AuthorizationHelper;
 
 public class AuthorizationHelper {
 	public static (string username, string password) GetBasicCredentials(HttpRequest request, ILogger logger = null) {
-		if (!request.Headers.ContainsKey("Authorization")) return (null, null);
+		if (!request.Headers.ContainsKey("Authorization")) {
+			logger.Error("Basic auth header missing");
+			return (null, null);
+		}
 
 		var authHeader = request.Headers["Authorization"].ToString();
 
-		if (!authHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase)) return (null, null);
+		if (!authHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase)) {
+			logger.Error("Invalid basic auth header");
+			return (null, null);
+		}
 
 		var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Substring(6).Trim())).Split(':', 2);
 
-		if (credentials.Length != 2) return (null, null);
+		if (credentials.Length != 2) {
+			logger.Error("Invalid basic auth header");
+			return (null, null);
+		}
 
+		logger.Info("Basic credentials were extracted");
 		return (credentials[0], credentials[1]);
 	}
 
 	public static string GetBearerToken(HttpRequest request, ILogger logger = null) {
-		if (!request.Headers.ContainsKey("Authorization")) return null;
+		if (!request.Headers.ContainsKey("Authorization")) {
+			logger.Error("Bearer auth header missing");
+			return null;
+		}
 
 		var authHeader = request.Headers["Authorization"].ToString();
 
-		if (!authHeader.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase)) return null;
+		if (!authHeader.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase)) {
+			logger.Error("Invalid bearer auth header");
+			return null;
+		}
 
+		logger.Info("Bearer token was extracted");
 		return authHeader.Substring(7).Trim();
 	}
 
@@ -40,11 +57,17 @@ public class AuthorizationHelper {
 
 	public static (string username, string realm, string nonce, string uri, string response, string opaque) GetDigestCredentials
 		(HttpRequest request, ILogger logger = null) {
-		if (!request.Headers.ContainsKey("Authorization")) return (null, null, null, null, null, null);
+		if (!request.Headers.ContainsKey("Authorization")) {
+			logger.Error("Digest auth header missing");
+			return (null, null, null, null, null, null);
+		}
 
 		var authHeader = request.Headers["Authorization"].ToString();
 
-		if (!authHeader.StartsWith("Digest ", StringComparison.OrdinalIgnoreCase)) return (null, null, null, null, null, null);
+		if (!authHeader.StartsWith("Digest ", StringComparison.OrdinalIgnoreCase)) {
+			logger.Error("Invalid digest auth header");
+			return (null, null, null, null, null, null);
+		}
 
 		var digestValues = authHeader.Substring(7).Trim().Split(',');
 
@@ -57,6 +80,7 @@ public class AuthorizationHelper {
 		digestDict.TryGetValue("response", out var response);
 		digestDict.TryGetValue("opaque", out var opaque);
 
+		logger.Info("Digest auth was extracted");
 		return (username, realm, nonce, uri, response, opaque);
 	}
 }
